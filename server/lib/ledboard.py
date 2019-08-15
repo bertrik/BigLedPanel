2349a3088728d627539192cb39514c4b4ff5b68f
folkert <folkert@vanheusden.com>
2019-08-15 13:50:45 +0200

    small performance tweak (that is: less cpu-usage)

--- server/lib/ledboard.py
+++ server/lib/ledboard.py
@@ -4,7 +4,7 @@ import time
 class Ledboard:
     def __init__(self,port,speed):
         self.ser = serial.Serial(port, speed, timeout=1)
-        self.framebuffer = [0x00] * 90
+        self.framebuffer = bytearray([0x00] * 92)
         time.sleep(1)
 
     def writebuffer(self, data):
@@ -13,37 +13,29 @@ class Ledboard:
             self.draw()
 
     def drawstring(self, string, font):
-        i = 0
-        self.framebuffer = [0x00] * 90
+        self.framebuffer = bytearray([0x00] * 92)
+        self.framebuffer[0] = 0x81
+        self.framebuffer[1] = 0x80
+
+        i = 2
         for char in string:
-            self.framebuffer[i] = font[ord(char)-32][0]
+            c = ord(char) - 32
+            self.framebuffer[i] = font[c][0]
             i += 1
-            self.framebuffer[i] = font[ord(char)-32][1]
+            self.framebuffer[i] = font[c][1]
             i += 1
-            self.framebuffer[i] = font[ord(char)-32][2]
+            self.framebuffer[i] = font[c][2]
             i += 1
-            self.framebuffer[i] = font[ord(char)-32][3]
+            self.framebuffer[i] = font[c][3]
             i += 1 
-            self.framebuffer[i] = font[ord(char)-32][4]
+            self.framebuffer[i] = font[c][4]
             i += 1 
+
         self.draw() 
  
     def draw(self):
-        self.ser.write(chr(0x81))
-        self.ser.write(chr(0x80))
-
-        for frame in self.framebuffer:
-        #    while self.ser.out_waiting > 0:
-        #        print self.ser.out_waiting
-                #time.sleep(.30)
-
-            self.ser.write(chr(frame))
-
-        #time.sleep(.20)
-
+        self.ser.write(self.framebuffer)
         self.ser.reset_input_buffer()
-            
-        self.ser.flush()
 
     def demo(self):
         for i in range(0,89):
